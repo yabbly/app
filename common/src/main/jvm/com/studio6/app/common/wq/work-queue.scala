c4f1ab8490d71a410f941c9471f250c89b8b9c45
Ian Shafer <ian@yabbly.com>
2014-07-15 17:05:40 -0700

    wip

--- common/src/main/jvm/com/studio6/app/common/wq/work-queue.scala
+++ common/src/main/jvm/com/studio6/app/common/wq/work-queue.scala
@@ -11,6 +11,11 @@ import redis.clients.jedis.Jedis
 import redis.clients.jedis.JedisPool
 import redis.clients.jedis.exceptions.JedisConnectionException
 
+import java.util.concurrent.Executors
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit._
+
 trait RedisClient {
   def pool: JedisPool
   def db: Option[Int] = None
@@ -66,4 +71,22 @@ abstract class RedisWorkQueue[T](
   protected def serialize(item: T): Array[Byte]
 
   protected def deserialize(bytes: Array[Byte]): T
+}
+
+trait SingleThreadExecutorWorkQueueProcessor extends Runnable {
+  def threadName: String
+
+  val executor = Executors.newSingleThreadExecutor(new ThreadFactory {
+    override def newThread(r: Runnable) = {
+      val t = new Thread(r, threadName)
+      t.setDaemon(true)
+      t
+    }
+  })
+
+  executor.submit(this)
+
+  override def run(): Unit = {
+    // Pick items off the work queue and process them
+  }
 }
